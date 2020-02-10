2f986d0d38d92e8a1b2f447b10f53e083db41a92
LedoKun <romamp100@gmail.com>
2020-02-10 21:55:06 +0700

    Early implementation of ICD10 typeahead

--- app/components/icd10-typeahead.js
+++ app/components/icd10-typeahead.js
@@ -1,24 +1,23 @@
 import TypeAhead from 'hospitalrun/components/type-ahead';
+import icd10Generator from 'hospitalrun/utils/icd10';
 export default TypeAhead.extend({
   class: 'scrollable-typeahead',
   minlength: 2,
-  selectionKey: 'id',
+  // selectionKey: 'id',
   setOnBlur: true,
   templates: {
     header: '<div class="alert alert-success well-sm query-results" role="alert"></div>'
   },
 
-  _sourceQuery(query, cb) { // Custom source function
-    // Get the data from the Blodhound engine and process it.
-    this.bloodhound.get(query, function(suggestions) {
-      cb(suggestions);
-      // Set the headers content.
-      let $header = this.$('.query-results');
-      $header.html(`<strong><em>${query}</em></strong> returned <strong>${suggestions.length}</strong> results`);
-    }.bind(this));
-  },
-
   _getSource() {
-    return this._sourceQuery.bind(this);
+    let ICD10Bloodhound = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace(this.get('displayKey')),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: icd10Generator(),
+      limit: 100
+    });
+    ICD10Bloodhound.initialize();
+    this.set('bloodhound-icd10', ICD10Bloodhound);
+    return ICD10Bloodhound.ttAdapter();
   }
 });
